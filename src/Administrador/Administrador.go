069b4d0928cab06ef71c7c722c8c556a0fc1b39f
Rodrigo Fernando Velázquez Cruz <rodrigo_velazquez99@ciencias.unam.mx>
2018-10-16 00:17:10 -0500

    Agregando busquedas

--- src/Administrador/Administrador.go
+++ src/Administrador/Administrador.go
@@ -6,46 +6,76 @@ import(
    _ "github.com/mattn/go-sqlite3"
 )
 
-var db *sql.DB
-
-func main()  {
-  IniciaBase()
-  BuscaCancion("touch")
+type Columna struct {
+  titulo string
+  interprete string
+  album string
+  genero string
+  ruta string
 }
 
-func IniciaBase(){
+
+func main()  {
+  base := IniciaBase()
+  BuscaCancion(base, "s")
+}
+
+func IniciaBase() *sql.DB {
   nuevaBase, err := sql.Open("sqlite3", "../Base/base.db")
   if err != nil {
     panic(err)
   }
-  db = nuevaBase
+  return nuevaBase
 }
 
-func BuscaCancion(solicitud string) (string, string, string ) {
-  rows, err := db.Query("SELECT path, title, genre FROM rolas WHERE title=?","City")
+func BuscaCancion(db *sql.DB, solicitud string) []Columna {
+  columnas:= make([]Columna, 0)
+  rows, err := db.Query("SELECT path, title, genre, id_performer, id_album FROM rolas WHERE title LIKE '%" + solicitud + "%'")
   if err != nil {
     panic(err)
   }
-  var nombre, genero, ruta string
+  var title, genre, path string
+  var idAlbum, idPerformer int
   for rows.Next() {
-    rows.Scan(&ruta, &nombre, &genero)
-    fmt.Println(ruta + " " + nombre + " " + " " + genero)
+    rows.Scan(&path, &title, &genre, &idPerformer ,&idAlbum)
+    performer := buscaInterprete(idPerformer,db )
+    Album := buscaAlbum(idAlbum,db )
+    nuevaColumna := Columna{
+      titulo: title,
+      interprete: performer,
+      album: Album,
+      genero: genre,
+      ruta: path,
+    }
+    columnas = append(columnas, nuevaColumna)
   }
-
+  rows.Close()
+  fmt.Println(columnas)
+  return columnas
 }
 
-func buscaTitulo(cadena string)  {
-
+func buscaInterprete(id int, db *sql.DB) string {
+  tabla,err := db.Query("SELECT name FROM performers WHERE id_performer=?",id)
+  if err != nil {
+    panic(err)
+  }
+  var interprete string
+  if tabla.Next() {
+    tabla.Scan(&interprete)
+  }
+  tabla.Close()
+  return interprete
 }
 
-func buscaInterprete(cadena string)  {
-
-}
-
-func buscaGenero(cadena string) {
-
-}
-
-func buscaAlbum(cadena string)  {
-
+func buscaAlbum(id int, db *sql.DB) string {
+  tabla,err := db.Query("SELECT name FROM albums WHERE id_album=?",id)
+  if err != nil {
+    panic(err)
+  }
+  var album string
+  if tabla.Next() {
+    tabla.Scan(&album)
+  }
+  tabla.Close()
+  return album
 }
